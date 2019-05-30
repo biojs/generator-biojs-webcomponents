4c82645bf62bbf7323b601232f074b4fe7d38330
Nikhil Vats <f20170473@pilani.bits-pilani.ac.in>
2019-05-30 11:58:10 +0530

    Added unit tests (#15)
    This adds two unit tests, for checking 
    - if the project is created with correct user inputs if user inputs something
    - if the project is created with default prompts if user inputs nothing.

--- __tests__/app.js
+++ __tests__/app.js
@@ -6,7 +6,8 @@ const helpers = require("yeoman-test");
 describe("generator-biojs-webcomponents:app", () => {
   beforeAll(() => {
     return helpers.run(path.join(__dirname, "../generators/app")).withPrompts({
-      someAnswer: true
+      toolNameComputer: "test-component-tool",
+      toolNameHuman: "Biojs test component"
     });
   });
 
@@ -25,5 +26,38 @@ describe("generator-biojs-webcomponents:app", () => {
   });
   it("initialises server script", () => {
     assert.file(["dev/serve.js"]);
+  });
+  it("initialises the component with correct user inputs", () => {
+    assert.fileContent([
+      [
+        "examples/index.html",
+        '<test-component-tool geneId="BRCA1"></test-component-tool>'
+      ],
+      ["examples/index.html", "<h1>Biojs test component demo</h1>"],
+      ["src/index.js", "define('test-component-tool', BiojsTestComponent);"],
+      ["src/style.less", ".BiojsTestComponent"]
+    ]);
+  });
+  it("initialises the component with default names if user inputs nothing", () => {
+    return helpers
+      .run(path.join(__dirname, "../generators/app"))
+      .withPrompts({
+        toolNameComputer: "",
+        toolNameHuman: ""
+      })
+      .then(() => {
+        assert.fileContent([
+          [
+            "examples/index.html",
+            '<biojs-webcomponent-tool-name-here geneId="BRCA1"></biojs-webcomponent-tool-name-here>'
+          ],
+          ["examples/index.html", "<h1>BioJS component demo</h1>"],
+          [
+            "src/index.js",
+            "define('biojs-webcomponent-tool-name-here', BioJSComponent);"
+          ],
+          ["src/style.less", ".BioJSComponent"]
+        ]);
+      });
   });
 });
